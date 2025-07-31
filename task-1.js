be65469cb97a0d326ff763182192a1fc7a32fab8
khandakershahi <khandakershahi@gmail.com>
2025-07-31 19:28:22 +0600

    initial commit

---
+++ task-1.js
@@ -0,0 +1,8 @@
+/**
+ * ### 1. Declare an array 
+1. Declare an array with 5 elements containing fruits
+2. console log the 3rd index element
+3. change the value of the 2nd index element to jambura
+4. console log the final array
+ */
+
