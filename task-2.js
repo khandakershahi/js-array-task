529e0bc105360236b2ca03290443e1722db016f6
khandakershahi <khandakershahi@gmail.com>
2025-08-01 17:10:15 +0600

    task-2 done

---
+++ task-2.js
@@ -0,0 +1,19 @@
+/**
+ * ### 2. Add or remove elements
+1. Declare an array of 3 tourist destinations
+2. Add a new tourist destination to your tourist array
+3. Add two more to your array
+4. Remove the last tourist destination you have added
+5. display the final array as output
+ */
+
+const touristDestination = ['Sundarban', 'Cox-bazar', 'Paharpur'];
+
+touristDestination.push('Bodho Bihar', 'Khagrachori');
+
+
+console.log(touristDestination);
+
+touristDestination.pop();
+
+console.log(touristDestination);
