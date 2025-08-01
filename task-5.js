5dc9d9d35a38e3f0d4916236421cf8dd264ea3d3
khandakershahi <khandakershahi@gmail.com>
2025-08-01 17:24:20 +0600

    task-5 done

---
+++ task-5.js
@@ -0,0 +1,23 @@
+/**
+ * ### 5. Combining Arrays
+
+**Instructions:**
+
+1. Create two arrays of your choice.
+2. Use the concat method to combine the two arrays into a new array.
+3. Print both the original arrays and the combined array using console.log().
+
+ */
+
+const arrayFirst = ['HTML5', 'CSS3', 'Javascript', 'React', 'MongoDB'];
+
+const arraySecond = ['Human', 'Animal', 'Bird', 'Ant'];
+
+const joinArry = arrayFirst.concat(arraySecond);
+
+
+console.log(arrayFirst);
+
+console.log(arraySecond);
+
+console.log(joinArry);
